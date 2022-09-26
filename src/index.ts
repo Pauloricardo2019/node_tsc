e457ba72c84e1b2b67048d84e3b832d107827a60
Pauloricardo2019 <paullin_santos13@hotmail.com>
2022-09-26 15:18:45 -0300

    typescript_apiExample

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
-import express from 'express'
+import express from 'express';
 
-const server = express()
+import { userRouter } from './modules/user/userRouter.js';
+
+const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.send("Hello world!")
-})
+  res.send('Hello world!');
+});
+
+server.use('/user', userRouter);
 
 server.listen(3000, () => {
-  console.log("Server started on port 3000")
-})
+  console.log('Server started on port 3000');
+});
---
+++ src/modules/user/userController.ts
@@ -0,0 +1,52 @@
+import { randomUUID } from 'crypto';
+import { Request, Response } from 'express';
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const userMemory: IUser[] = [];
+
+export const userIndex = async (req: Request, res: Response) => {
+  res.json(userMemory);
+};
+
+export const userCreate = async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  const id = randomUUID();
+
+  const user: IUser = {
+    id,
+    name,
+    email,
+  };
+
+  userMemory.push(user);
+
+  res.json(user);
+};
+
+export const userShow = async (req: Request, res: Response) => {
+  const { user_id } = req.params;
+
+  const user = userMemory.find((user) => {
+    user.id == user_id;
+  });
+
+  res.json(user);
+};
+
+export const userDelete = async (req: Request, res: Response) => {
+  const { user_id } = req.params;
+
+  const userIndex = userMemory.findIndex((user) => {
+    user.id == user_id;
+  });
+
+  userMemory.splice(userIndex, 1);
+
+  res.json({ message: 'User deleted' });
+};
